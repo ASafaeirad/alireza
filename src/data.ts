905dcccfa015deac29c257854121b3512fdd6464
Alireza Safaierad <frontendmonster@gmail.com>
2021-09-27 18:41:17 +0400

    update about section

--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,7 @@
 export const data = {
   name: 'Alireza',
   about:
-    'Innovative Front End Engineer with +5 years of professional experience, focused on modular and scalable design and development. Proficient in Web technologies and modern JS/TS echosystem. Passionate about learning, teaching, and, mentoring and code review.',
+    'Innovative Front End Engineer with +5 years of professional experience, focused on modular and scalable design and development. Proficient in Web technologies and modern JS/TS ecosystem. I am passionate about learning, teaching, mentoring, and code review.',
   greetings: `Hi 👋, My name is`,
   github: 'https://github.com/frontendmonster',
   linkedin: 'https://www.linkedin.com/in/alireza-safaeirad',
