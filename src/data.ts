f1a9ccc84656001213036400f86d80da1cc99734
Alireza Safaierad <frontendmonster@gmail.com>
2021-11-20 14:23:53 +0400

    update about me

--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,10 @@
 export const data = {
   name: 'Alireza',
   about:
-    'Innovative FrontEnd Engineer with +5 years of professional experience, focused on modular and scalable design and development. Proficient in Web technologies and modern JS/TS ecosystem. I am passionate about learning, teaching, mentoring, and code review.',
+    'Innovative FrontEnd Engineer with +10 years of programming experience and over five years of professional experience, ' +
+    'focused on modular and scalable design and development.' +
+    'Proficient in Web technologies and modern JS/TS ecosystem.' +
+    'I am passionate about learning, teaching, and mentoring.',
   greetings: `Hi 👋, My name is`,
   github: 'frontendmonster',
   linkedin: 'alireza-safaeirad',
